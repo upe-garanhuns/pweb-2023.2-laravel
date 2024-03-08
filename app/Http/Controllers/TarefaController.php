92ffbc150630ebe67df1b074da68a7c3849f81dc
Mateus Almeida <mateusva.1132@gmail.com>
2024-03-08 18:38:06 -0300

    feat: TarefaController e função create()

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    public function create(){
+        return view('tarefa.create');
+    }
+}
