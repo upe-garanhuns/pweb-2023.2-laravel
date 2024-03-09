f2ce6ccd3d6eacdac56037d97213cd946ec14d93
Pedro H. Mota <110642021+mota-pedro@users.noreply.github.com>
2024-03-09 15:18:58 -0300

    fix: correção de nome das colunas da tabela da View

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,26 +6,26 @@
     <title>Criar tarefas</title>
 </head>
 <body>
-        
+
 
     <h2>Criar tarefas</h2>
 
     <form action="">
         @csrf
 
-        
+
         <label for="">Id: </label>
-        <input type="number" name = "nome" id = "nome" placeholder="Id da conta"><br><br>
+        <input type="number" name = "id" id = "id" placeholder="Id da conta"><br><br>
 
         <label for="">Nome: </label>
         <input type="text" name = "nome" id = "nome" placeholder="Nome da tarefa" required><br><br>
 
         <label for="">Descrição: </label>
-        <input type="text" name = "descrição" id = "descrição" placeholder="Descrição da tarefa"><br><br>
-        
+        <input type="text" name = "descricao" id = "descricao" placeholder="Descrição da tarefa"><br><br>
+
         <label for="">Data: </label>
         <input type="date" name = "data" id = "data" placeholder="Data da tarefa"><br><br>
-        
+
         <label for="">Concluído: </label>
         <input type="checkbox" name = "concluido" id = "concluido" placeholder="Checkbox de concluido"><br><br>
 
