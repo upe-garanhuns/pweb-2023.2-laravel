ab26531e739a2269d874894adf2a5658239853c3
Pedro H. Mota <110642021+mota-pedro@users.noreply.github.com>
2024-03-09 15:17:16 -0300

    feat: função store() para armazenar tarefas

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -10,6 +10,14 @@ class TarefaController extends Controller
         return view('create');
     }
 
+    public function store(Request $request) {
+        dd($request->id);
+        dd($request->nome);
+        dd($request->descricao);
+        dd($request->data);
+        dd($request->concluido);
+    }
+
     public function show() {
         return view('show');
     }
